95e69d48674833f5b674534342338c9c69c05676
sivanantham palaniappan <113528354+sivananthampalaniappan@users.noreply.github.com>
2023-04-28 07:29:02 +0530

    Add files via upload

---
+++ DADDA MULTIPLIER/daddatb.v
@@ -0,0 +1,32 @@
+module daddatb;
+
+  wire [15:0] s;
+
+  reg [7:0]a,b;
+
+  reg clk;
+
+  dadda uut (s,a,b);
+
+  initial
+
+    begin
+
+      #5 clk=1'b0;
+
+      #5 clk=1'b1;
+
+    end
+
+
+      always @(posedge clk)
+
+     begin
+
+       #10 a[7:0]=8'b11111111;b[7:0]=8'b11111111;
+
+       #10 $stop;
+
+     end
+
+endmodule
